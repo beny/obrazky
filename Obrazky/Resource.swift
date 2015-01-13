7ebc9a27e1100d9d44959cafc60a001862c2a972
Ondra Benes <ondra.benes@gmail.com>
2015-01-13 04:50:52 +0100

    Refactored inner HTML parsing

--- Obrazky/Resource.swift
+++ Obrazky/Resource.swift
@@ -13,12 +13,80 @@ class Resource {
     let session: AFURLSessionManager
 
     init() {
+        // create session
         session = AFURLSessionManager(sessionConfiguration: NSURLSessionConfiguration.defaultSessionConfiguration())
         session.responseSerializer = AFHTTPResponseSerializer()
     }
 
-    func parseData(response: AnyObject?) {
-        
+    class func parseData(data: AnyObject?) -> (Array<Image>?) {
+
+        // helper closure to find specific
+        func findPattern(pattern: String, string: String) -> Array<String>? {
+            var error: NSError?
+            let regexTitle = NSRegularExpression(pattern: pattern, options: nil, error: &error)
+            if (error != nil) {
+                return nil
+            }
+            let matchesResults = regexTitle?.matchesInString(string, options: nil, range: NSMakeRange(0, string.utf16Count)) as Array<NSTextCheckingResult>
+            let matches: Array<String> = matchesResults.map { NSString(string: string).substringWithRange($0.rangeAtIndex(1)) }
+
+            return matches
+        }
+
+        if let responsObject = data as? NSDictionary {
+            if let result = responsObject["result"] as? NSDictionary {
+                if let boxes = result["boxes"] as? NSString {
+
+                    // title anchorlike'>(.*?)</span>
+                    // size <span>(.*?)</span>
+                    // originURL dot=\"(.*?)\"
+                    // obrazkyURL src=\"(.*?)&
+
+                    let titles = findPattern("anchorlike'>(.*?)</span>", boxes)
+                    let sizes = findPattern("<span>(.*?)</span>", boxes)
+                    let originURLs = findPattern("dot=\"(.*?)\"", boxes)
+                    let obrazkyURLs = findPattern("src=\"(.*?)&", boxes)
+
+//                    var error: NSError?
+//                    let regexTitle = NSRegularExpression(pattern: "anchorlike'>(.*?)</span>", options: nil, error: &error)
+//                    let matchesTitle = regexTitle?.matchesInString(boxes, options: nil, range: NSMakeRange(0, boxes.length)) as Array<NSTextCheckingResult>
+//                    let titles: Array<String> = matchesTitle.map { boxes.substringWithRange($0.rangeAtIndex(1)) }
+//
+//                    error = nil
+//                    let regexSize = NSRegularExpression(pattern: "<span>(.*?)</span>", options: nil, error: &error)
+//                    let matchesSize = regexSize?.matchesInString(boxes, options: nil, range: NSMakeRange(0, boxes.length)) as Array<NSTextCheckingResult>
+//                    let sizes: Array<String> = matchesSize.map { boxes.substringWithRange($0.rangeAtIndex(1)) }
+//
+//                    error = nil
+//                    let regexOriginURL = NSRegularExpression(pattern: "dot=\"(.*?)\"", options: nil, error: &error)
+//                    let matchesOriginURL = regexOriginURL?.matchesInString(boxes, options: nil, range: NSMakeRange(0, boxes.length)) as Array<NSTextCheckingResult>
+//                    let originURLs: Array<String> = matchesOriginURL.map { boxes.substringWithRange($0.rangeAtIndex(1)) }
+//
+//                    error = nil
+//                    let regexObrazkyURL = NSRegularExpression(pattern: "src=\"(.*?)&", options: nil, error: &error)
+//                    let matchesObrazkyURL = regexObrazkyURL?.matchesInString(boxes, options: nil, range: NSMakeRange(0, boxes.length)) as Array<NSTextCheckingResult>
+//                    let obrazkyURLs: Array<String> = matchesObrazkyURL.map { boxes.substringWithRange($0.rangeAtIndex(1)) }
+
+                    if let titles = titles {
+                        if let sizes = sizes {
+                            if let originURLs = originURLs {
+                                if let obrazkyURLs = obrazkyURLs {
+                                    var array = [Image]()
+                                    for (index, title) in enumerate(titles) {
+                                        let image = Image(title: title, size: sizes[index], obrazkyURL: obrazkyURLs[index], originURL: originURLs[index])
+                                        array.append(image)
+                                    }
+
+                                    return array
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return nil
     }
 
 }
--- Obrazky/RootViewController.swift
+++ Obrazky/RootViewController.swift
@@ -39,42 +39,11 @@ class RootViewController: UICollectionViewController, UISearchBarDelegate, UICol
 
                 var error: NSError?
                 let JSON: AnyObject? = NSJSONSerialization.JSONObjectWithData(fixedData!, options: NSJSONReadingOptions.MutableContainers, error: &error)
-                if let responseObject = JSON as? NSDictionary {
-                    if let result = responseObject["result"] as? NSDictionary {
-                        if let boxes = result["boxes"] as? NSString {
 
-                            // title anchorlike'>(.*?)</span>
-                            // size <span>(.*?)</span>
-                            // originURL dot=\"(.*?)\"
-                            // obrazkyURL src=\"(.*?)&
-
-                            var error: NSError?
-                            let regexTitle = NSRegularExpression(pattern: "anchorlike'>(.*?)</span>", options: nil, error: &error)
-                            let matchesTitle = regexTitle?.matchesInString(boxes, options: nil, range: NSMakeRange(0, boxes.length)) as Array<NSTextCheckingResult>
-                            let titles: Array<String> = matchesTitle.map { boxes.substringWithRange($0.rangeAtIndex(1)) }
-
-                            error = nil
-                            let regexSize = NSRegularExpression(pattern: "<span>(.*?)</span>", options: nil, error: &error)
-                            let matchesSize = regexSize?.matchesInString(boxes, options: nil, range: NSMakeRange(0, boxes.length)) as Array<NSTextCheckingResult>
-                            let sizes: Array<String> = matchesSize.map { boxes.substringWithRange($0.rangeAtIndex(1)) }
-
-                            error = nil
-                            let regexOriginURL = NSRegularExpression(pattern: "dot=\"(.*?)\"", options: nil, error: &error)
-                            let matchesOriginURL = regexOriginURL?.matchesInString(boxes, options: nil, range: NSMakeRange(0, boxes.length)) as Array<NSTextCheckingResult>
-                            let originURLs: Array<String> = matchesOriginURL.map { boxes.substringWithRange($0.rangeAtIndex(1)) }
-
-                            error = nil
-                            let regexObrazkyURL = NSRegularExpression(pattern: "src=\"(.*?)&", options: nil, error: &error)
-                            let matchesObrazkyURL = regexObrazkyURL?.matchesInString(boxes, options: nil, range: NSMakeRange(0, boxes.length)) as Array<NSTextCheckingResult>
-                            let obrazkyURLs: Array<String> = matchesObrazkyURL.map { boxes.substringWithRange($0.rangeAtIndex(1)) }
-
-                            self.images.removeAll(keepCapacity: false)
-                            for (index, title) in enumerate(titles) {
-                                let image = Image(title: title, size: sizes[index], obrazkyURL: obrazkyURLs[index], originURL: originURLs[index])
-                                self.images.append(image)
-                            }
-                        }
-                    }
+                // update images
+                self.images.removeAll(keepCapacity: false)
+                if let images = Resource.parseData(JSON) {
+                    self.images = images
                 }
             }
             self.collectionView?.reloadData()
@@ -122,7 +91,7 @@ class RootViewController: UICollectionViewController, UISearchBarDelegate, UICol
         var width = screenSize.width
         let margin: CGFloat = 8
 
-        // for smaller screens on portrait
+        // for smaller screens on portrait, for larger screens 
         if screenSize.width < 400 {
             width = width - 2 * margin
         }
